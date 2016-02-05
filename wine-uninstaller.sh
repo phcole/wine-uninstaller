d3caf0841283bf9675baeb92baf9d80b3b56a843
Peng <penghao@linuxdeepin.com>
2016-02-05 15:03:29 +0800

    add script to process desktop file
    Signed-off-by: Peng <penghao@linuxdeepin.com>

---
+++ wine-uninstaller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+UNINSTALLER_EXE="app-uninstaller.exe"
+
+Desktop_File=$1
+if [ -z $Desktop_File ]; then
+	echo "no desktop given."
+	exit 1
+fi
+
+Exec=$(cat "$Desktop_File" | grep 'Exec=')
+# we suppose this is a CrossOver desktop file. WINE support will be later.
+Exec=${Exec#Exec=\"}
+Exec=${Exec%\" %u}
+Exec=$(cat $Exec | grep 'exec ')
+Bottle=${Exec#*--bottle \"}
+Bottle=${Bottle%%\" *}
+linkfile=${Exec#*--start \"}
+linkfile=${linkfile%%\" *}
+
+echo "cxoffice bottle: \"$Bottle\""
+echo "shell link file: \"$linkfile\""
+
+/opt/cxoffice/bin/wine --bottle="$Bottle"  --wait-children $UNINSTALLER_EXE -- "$linkfile"

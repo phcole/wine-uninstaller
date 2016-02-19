da082882967592dc9927d8a7a7b5df894f59551b
Peng <penghao@linuxdeepin.com>
2016-02-19 15:37:35 +0800

    Add Copyright info.
    Signed-off-by: Peng <penghao@linuxdeepin.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright (C) 2016 Deepin Technology Co., Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
 UNINSTALLER_EXE='app-uninstaller.exe'
 BASE_DIR=$(cd `dirname $0`; pwd)
 
